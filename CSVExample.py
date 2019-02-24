1ec787e797f323557bc23d3285940462025df1a7
Nejc <dev.neetsu@gmail.com>
2019-02-24 22:35:54 +0100

    Update CSV Example

--- CSVExample.py
+++ CSVExample.py
@@ -1,10 +1,18 @@
 import csv
 
-with open('example.csv') as csvfile:
+# example how to process and wotk with CSV data
+#File Location
+csvF = 'D:\Development Proeckts\Python-Basic-Totorial\example.csv'
+#Location of the run
+from os import getcwd ; print(getcwd())
+#Working with the file
+with open(csvF ) as csvfile:
+    #Work with CSV File
     readCSV = csv.reader(csvfile, delimiter=',')
     dates = []
     colors = []
     for row in readCSV:
+        #Row per row work
         print(row)
         print(row[0])
         print(row[0],row[1],row[2],)
@@ -13,17 +21,12 @@ with open('example.csv') as csvfile:
 
         dates.append(date)
         colors.append(color)
-
     print(dates)
     print(colors)
-
-    # now, remember our lists?
-
     try:
         whatColor = input('What color do you wish to know the date of?:')
         coldex = colors.index(whatColor)
         theDate = dates[coldex]
         print('The date of',whatColor,'is:',theDate)
-
     except Exception as e:
         print(e)
