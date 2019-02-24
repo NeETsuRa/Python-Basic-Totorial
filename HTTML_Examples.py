e3cfba9027f68d15f353d018b4a7d20f0a0d217a
Nejc <dev.neetsu@gmail.com>
2019-02-24 23:06:22 +0100

    HTML Example Code

--- HTTML_Examples.py
+++ HTTML_Examples.py
@@ -1,19 +1,18 @@
 import socket
 
+#Working with sockets
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(s)
-
 server = 'pythonprogramming.net'
-
 port = 80
-
+# Get info from an server
 server_ip = socket.gethostbyname(server)
 print(server_ip)
 
 request = "GET / HTTP/1.1\nHost: "+server+"\n\n"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("pythonprogramming.net", 80))
-
+# http request to an server
 s.send(request.encode())
 result = s.recv(4096)
 
@@ -22,6 +21,7 @@ while (len(result) > 0):
     print(result)
     result = s.recv(4096)
 
+# Port scaner of an web site
 #target = input('What website to scan?: ')
 target = 'https://www.hackthissite.org/'
 def pscan(port):
@@ -36,5 +36,6 @@ for x in range(25):
     if pscan(x):
         print('Port',x,'is open')	
 
+# How to execute code provided as Text.
 exec("def test(): print('oooo snap!!!')")
 test()
