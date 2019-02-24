6f6c3807035eb158f759cafed2b0e9d218142368
Nejc <dev.neetsu@gmail.com>
2019-02-24 23:39:29 +0100

    FTP Access

--- FTPAccess.py
+++ FTPAccess.py
@@ -7,19 +7,16 @@ ftp.login(user='username', passwd = 'password')
 ftp.cwd('/whyfix/')
 # download a file
 def grabFile():
-
     filename = 'example.txt'
-
     localfile = open(filename, 'wb')
     ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
-
     ftp.quit()
     localfile.close()
 #upload a file
 def placeFile():
-
     filename = 'exampleFile.txt'
     ftp.storbinary('STOR '+filename, open(filename, 'rb'))
     ftp.quit()
-
+    
 placeFile()
+grabFile()
