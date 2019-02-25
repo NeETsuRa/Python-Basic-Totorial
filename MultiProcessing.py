ff16e3c545f7b0e8d1ad86b4cb36c3407c147447
Nejc <dev.neetsu@gmail.com>
2019-02-25 21:11:02 +0100

    Multiprocessing Example

--- MultiProcessing.py
+++ MultiProcessing.py
@@ -5,21 +5,24 @@ from multiprocessing import Pool
 
 logging.basicConfig(filename='logfile.log',level=logging.DEBUG)
 
+# print/log numbers
 def spawn(num, num2):
     print('Spawn # {} {}'.format(num, num2))
     logging.info('spawn {} + {}'.format(str('str'), str('sst')))
 
-
+# return a double and log it
 def job(num):
     return num * 2
     logging.info('job {} + {}'.format(str('str'), str('sst')))
 
+#Main function of the Program
 if __name__ == '__main__':
     for i in range(500):
+        #multiprocessing part
         p = multiprocessing.Process(target=spawn, args=(i, i+1))
         p.start()
-    
     logging.info('main {} + {}'.format(str('str'), str('sst')))
+    #creating a pool for execution
     p = Pool(processes=20)
     data = p.map(job, [i for i in range(500)])
     p.close()
